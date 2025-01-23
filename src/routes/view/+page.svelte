d4f1d5055b2575584638d9bceaab84da878b8446
Quang Luong <luongquyquang@gmail.com>
2025-01-23 22:30:55 +0700

    Init chart from API response data for mode Edit

--- src/routes/view/+page.svelte
+++ src/routes/view/+page.svelte
@@ -4,18 +4,19 @@
   import { onMount } from 'svelte';
   import { page } from '$app/stores';
   import type { APIResponse } from '$lib/types';
+  import { derived } from 'svelte/store';
   // onMount(initHandler);
   let apiData: APIResponse = {
     code: ''
   }; // Variable to store API response
 
-  $: urlParameters = $page.url.searchParams; // Access URL search parameters
-  $: pageId = urlParameters.get('pageId');
-  $: contentId = urlParameters.get('contentId');
+  const urlParameters = derived(page, ($page) => $page.url.searchParams);
+  const pageId = derived(urlParameters, (parameters) => parameters.get('pageId'));
+  const contentId = derived(urlParameters, (parameters) => parameters.get('contentId'));
 
   onMount(async () => {
     try {
-      apiData = await fetchJSON(`/api/mermaid/content?pageId=${pageId}&contentId=${contentId}`); // Fetch data from the API
+      apiData = await fetchJSON(`/api/mermaid/content?pageId=${$pageId}&contentId=${$contentId}`); // Fetch data from the API
       initHandlerV2(apiData); // Run initHandler after the data is fetched
     } catch (error) {
       console.error('Failed to initialize:', error);
